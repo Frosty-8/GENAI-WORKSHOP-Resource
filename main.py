694ea585c72e3e5da9f8b48cd6235ba1d1c500cb
Sarthak <sarthakdongare611@gmail.com>
2025-07-20 15:23:20 +0530

    feat(ntbk): add fine_tuning_model and rag notebooks

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from genai-workshop!")
+import logging
+from rich.logging import RichHandler
 
+# Configure RichHandler with custom options
+logging.basicConfig(
+    level=logging.DEBUG,  # Show all levels
+    format="%(filename)s %(levelname)s | %(asctime)s | %(message)s",
+    handlers=[RichHandler(rich_tracebacks=True)]
+)
 
-if __name__ == "__main__":
-    main()
+logger = logging.getLogger()
+
+logger.debug("This is a debug message: invisible unless level is DEBUG.")
+logger.info("App started successfully.")
+logger.warning("Deprecated function is used here.")
+logger.error("An error occurred during processing.")
+logger.critical("Critical failure! Application will terminate.")
